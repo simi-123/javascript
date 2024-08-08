a5124b5d9a971d6305635ed08041f4223a6e875d
Simran Singh <111515120+simi-123@users.noreply.github.com>
2024-08-08 06:18:03 +0000

    commit the changes

---
+++ folder_02/01_array.js
@@ -0,0 +1,31 @@
+const myArray =[0,1,2,3,4,5]
+//array methods
+
+// myArray.push(6)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+
+// console.log(myArray.indexOf(9));
+
+const newArr = myArray.join()
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice splice
+
+console.log("a",myArray)
+
+const myn1= myArray.slice(1,3)
+
+console.log(myn1)
+
+console.log("b",myArray)
+
+const myn2= myArray.splice(1,3)
+console.log("c",myArray)
+console.log(myn2)
+
---
+++ folder_02/02_array.js
@@ -0,0 +1,8 @@
+const array = ["abc","cde","efg"]
+
+const array1 = ["ghi","ijk","klm"]
+
+array.push(array1)
+
+array.concat(array1)
+console.log(array)
